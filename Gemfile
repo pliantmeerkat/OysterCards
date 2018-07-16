985ce4e5b420ad55cb9fd71bc61b69957249d989
Jack Branch <jackbranch@Jacks-MacBook-Pro.local>
2018-07-16 17:45:05 +0100

    end of step 7

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source "https://rubygems.org"
-
+source ruby '2.5.1'
 
 git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
--- spec/Oyster_spec.rb
+++ spec/Oyster_spec.rb
@@ -7,16 +7,41 @@ describe Oyster do
   end
   context 'Feature 2 - Top Up' do
     describe '#top_up' do
-      before (:each) { subject.top_up(1.00) }
-      it { expect(subject).to respond_to(:top_up).with(1).arguments }
+      before(:each) { subject.top_up(1.00) }
+      it { expect(subject).to respond_to(:top_up).with(1).argument }
       it { expect(subject.balance).to eq(1.0) }
       it 'raises error if too much is topped up' do
         max_bal = Oyster::MAXIMUM_BALANCE
         expect { subject.top_up(100) }.to raise_error(['cannot top up',
-                                                      'max balance of',
-                                                      "#{max_bal}",
-                                                      'reached'].join(" "))
+                                                       'max balance of',
+                                                       "#{max_bal}",
+                                                       'reached'].join(' '))
       end
+    end
+  end
+  context 'Feature 3 - Deduct' do
+    describe '#deduct' do
+      before(:each) { subject.balance = 10.00}
+      it { expect(subject).to respond_to(:deduct).with(1).argument }
+      it 'can deduct money from the card' do
+        subject.deduct(1.00)
+        expect(subject.balance).to eq(9.0)
+      end
+      it 'raises an error if not enough money availible' do
+        expect { subject.deduct(100) }.to raise_error(['cannot deduct',
+                                                       'not enough money',
+                                                       'on card'].join(' '))
+      end
+    end
+  end
+  context 'Feature 4 - Tap in' do
+    describe '#tap_in' do
+      it { expect(subject).to respond_to(:tap_in) }
+    end
+  end
+  context 'Featre 5 - Tap out' do
+    describe '#tap_out' do
+      it { expect(subject).to respond_to(:tap_out) }
     end
   end
 end
