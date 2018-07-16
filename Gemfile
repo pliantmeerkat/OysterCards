a192d5a9723586f247b77634ea5ed2c4d5e3840f
Jack Branch <jackbranch@Jacks-MacBook-Pro.local>
2018-07-16 17:27:22 +0100

    step 6 finish

--- Gemfile
+++ Gemfile
@@ -1,8 +1,8 @@
 source "https://rubygems.org"
-source ruby '2.2.3'
+
 
 git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
 group :development, :tesst do
-  gem "rspec""
+  gem "rspec"
 end
--- spec/Oyster_spec.rb
+++ spec/Oyster_spec.rb
@@ -1,4 +1,23 @@
-
+require './lib/Oyster'
 describe Oyster do
-
+  context 'Feature 1 - Balance' do
+    describe '#balance' do
+      it { expect(subject).to respond_to(:balance) }
+    end
+  end
+  context 'Feature 2 - Top Up' do
+    describe '#top_up' do
+      before (:each) { subject.top_up(1.00) }
+      it { expect(subject).to respond_to(:top_up).with(1).arguments }
+      it { expect(subject.balance).to eq(1.0) }
+      it 'raises error if too much is topped up' do
+        max_bal = Oyster::MAXIMUM_BALANCE
+        expect { subject.top_up(100) }.to raise_error(['cannot top up',
+                                                      'max balance of',
+                                                      "#{max_bal}",
+                                                      'reached'].join(" "))
+      end
+    end
+  end
 end
+
