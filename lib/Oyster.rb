7fb4577c2d8a65602cb92a502b34934e9ab924d5
Jack Branch <jackbranch@Jacks-MacBook-Pro.local>
2018-07-16 17:45:25 +0100

    end of step 7

---
+++ lib/Oyster.rb
@@ -0,0 +1,28 @@
+# Oyster
+class Oyster
+  MAXIMUM_BALANCE = 90
+  attr_accessor :balance
+  def initialize
+    @balance = 0
+    @errors = ["cannot top up max balance of #{MAXIMUM_BALANCE} reached",
+               'cannot deduct not enough money on card']
+  end
+
+  def top_up(amount)
+    return raise(@errors[0]) if amount + balance > MAXIMUM_BALANCE
+    @balance += amount
+  end
+
+  def deduct(amount)
+    return raise(@errors[1]) if balance - amount < 0
+    @balance -= amount
+  end
+
+  def tap_in
+
+  end
+
+  def tap_out
+
+  end
+end
